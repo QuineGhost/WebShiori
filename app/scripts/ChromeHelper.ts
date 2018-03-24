2c714d46b92146dd4ed4d168b8c85858b4faa414
quineghost <quineghost@gmail.com>
2018-03-24 16:05:32 +0900

    using promise

--- app/scripts/ChromeHelper.ts
+++ app/scripts/ChromeHelper.ts
@@ -4,52 +4,59 @@ class ChromeHelper {
       localStorageKeyName = '';
     }
 
-    // get current open tab info.
-    chrome.tabs.query(queryInfo, function (tabs) {
+    const promise = new Promise((resolve, reject) => {
 
-      try {
-        let currentTab: any = tabs.shift();
-        let requestObj = {
-          flg: flg,
-          message: message
+      chrome.tabs.query(queryInfo, function (tabs) {
+
+        let res = {
+          currentTab: tabs.shift(),
+          requestObj: {
+            flg: flg,
+            message: message
+          }
         };
 
-        let scrollTopValue = '';
+        resolve(res);
+      });
 
-        switch (flg) {
-          case 'top':
+    }).then((res: any) => {
 
-            // topボタン押した場合、contents側には何も送信しない
-            chrome.tabs.sendMessage(currentTab.id, requestObj, function (res: any) { });
-            break;
-          case 'regist':
+      let scrollTopValue = '';
 
-            chrome.tabs.sendMessage(currentTab.id, requestObj, (res: any) => {
-              console.log(res);
-            });
-            break;
+      switch (flg) {
+        case 'top':
 
-          case 'jump':
+          // topボタン押した場合、contents側には何も送信しない
+          chrome.tabs.sendMessage(res.currentTab.id, res.requestObj, function (res: any) { });
+          break;
+        case 'regist':
 
-            chrome.storage.local.get(function (keys) {
-              chrome.tabs.sendMessage(
-                currentTab.id,
-                {
-                  scrollTopValueByDomainList: keys.scrollTopValueByDomainList,
-                  flg: flg
-                },
-                function (res: Response) {
+          chrome.tabs.sendMessage(res.currentTab.id, res.requestObj, (res: any) => {
+            console.log(res);
+          });
+          break;
 
-                }
-              );
-            });
-            break;
-        }
+        case 'jump':
 
-      } catch (e) {
-        console.error(e.message);
+          chrome.storage.local.get(function (keys) {
+            chrome.tabs.sendMessage(
+              res.currentTab.id,
+              {
+                scrollTopValueByDomainList: keys.scrollTopValueByDomainList,
+                flg: flg
+              },
+              function (res: Response) {
+
+              }
+            );
+          });
+          break;
       }
+
+    }).catch((err) => {
+      console.log(err);
     });
+
   }
 }
 
--- app/scripts/contents.ts
+++ app/scripts/contents.ts
@@ -3,97 +3,15 @@ chrome.runtime.onMessage.addListener(
 
     if (request.flg === 'regist') {
 
-      // get scroll top value.
+      // regist scroll top value.
       console.log('on message from ' + request.message);
-
-      /*要素が重なっているとなぜ複数リクエストされちゃって(?)、
-      最終的に0で更新されてしまうから、
-      0のときはすぐリターンするようにしている
-      */
-      if ($(window).scrollTop() === 0) {
-        return;
-      }
-
-      let scrollTop = $(window).scrollTop();
-      let scrollTopValueByDomainList = new Array();
-
-      chrome.storage.local.get((keys) => {
-
-
-        // 今回の分も合わせてリスト作り直し
-        if (typeof keys !== 'undefined' && typeof keys.scrollTopValueByDomainList !== 'undefined') {
-
-          let index: number = 0;
-          for (let scrollTopValueByDomain of keys.scrollTopValueByDomainList) {
-
-            // 同じドメイン上書き
-            let flg: boolean = false;
-            if (scrollTopValueByDomain.domain === document.location.href.split('/')[2]) {
-              flg = true;
-            }
-
-            if (flg) {
-              keys.scrollTopValueByDomainList[index] = scrollTopValueByDomain;
-            } else {
-              scrollTopValueByDomainList.push(scrollTopValueByDomain);
-            }
-
-            index = index + 1;
-          }
-        }
-
-        scrollTopValueByDomainList.push({
-          domain: document.location.href.split('/')[2],
-          scrollTopValue: scrollTop
-        });
-
-        chrome.storage.local.set({ scrollTopValueByDomainList: scrollTopValueByDomainList }, function () {
-          console.log('complete localStorage value set');
-
-          let responseObj = {
-            msg: 'response',
-            scrollTop: scrollTop
-          };
-
-          sendResponse(responseObj);
-        });
-      });
+      regist(sendResponse);
 
     } else if (request.flg === 'jump') {
 
-      let shiori = '';
-
-      for (let scrollTopValueByDomain of request.scrollTopValueByDomainList) {
-        if (scrollTopValueByDomain.domain === document.location.href.split('/')[2]) {
-          shiori = scrollTopValueByDomain.scrollTopValue;
-        }
-      }
-
-      // 栞位置までscroll(オートスクロールの場合は4000ms)
-      let bodyHeight: any = $('body').height();
-      if (parseInt(shiori, 10) > bodyHeight) {
-        $('html, body').animate({ scrollTop: shiori }, 4000);
-      } else {
-        $('html, body').animate({ scrollTop: shiori }, 500);
-      }
-
-      // create shiori.
-      let activeShioriElement = $('#shiori-border');
-      if (typeof activeShioriElement !== 'undefined') {
-        activeShioriElement.remove();
-      }
-
-      let shioriElement = $('<img>', {
-        'id': 'shiori-border',
-        'src': chrome.extension.getURL('/images/shiori.png'),
-        'height': '50px'
-      }).appendTo('body');
-
-      $('#shiori-border').css({
-        top: parseInt(shiori),
-        position: 'absolute',
-        opacity: '0.3'
-      });
+      // jump scroll top value.
+      console.log('on message from ' + request.message);
+      jump(request, sendResponse);
 
     } else if (request.flg === 'top') {
 
@@ -103,3 +21,112 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+
+/**
+ * regist button
+ * @param sendResponse
+ */
+function regist(sendResponse: any) {
+  //  要素が重なっているとなぜ複数リクエストされちゃって(?)、
+  //  最終的に0で更新されてしまうから0のときは処理終了
+  if ($(window).scrollTop() === 0) {
+    return;
+  }
+
+  let scrollTop = $(window).scrollTop();
+  let scrollTopValueByDomainList = new Array();
+
+  const promise = new Promise((resolve, reject) => {
+
+    // get shiori value
+    chrome.storage.local.get((keys) => {
+
+      // 今回の分も合わせてリスト作り直し
+      if (typeof keys !== 'undefined' && typeof keys.scrollTopValueByDomainList !== 'undefined') {
+
+        let index: number = 0;
+        for (let scrollTopValueByDomain of keys.scrollTopValueByDomainList) {
+
+          // 同じドメイン上書き
+          let flg: boolean = false;
+          if (scrollTopValueByDomain.domain === document.location.href.split('/')[2]) {
+            flg = true;
+          }
+
+          if (flg) {
+            keys.scrollTopValueByDomainList[index] = scrollTopValueByDomain;
+          } else {
+            scrollTopValueByDomainList.push(scrollTopValueByDomain);
+          }
+
+          index = index + 1;
+        }
+      }
+
+      scrollTopValueByDomainList.push({
+        domain: document.location.href.split('/')[2],
+        scrollTopValue: scrollTop
+      });
+
+      resolve('succeed');
+    });
+
+  }).then((res) => {
+
+    chrome.storage.local.set({ scrollTopValueByDomainList: scrollTopValueByDomainList }, function () {
+      console.log('complete localStorage value set');
+
+      let responseObj = {
+        msg: 'response',
+        scrollTop: scrollTop
+      };
+
+      sendResponse(responseObj);
+    });
+
+  }).catch((err) => {
+    console.error(err);
+  });
+}
+
+/**
+ * jump button
+ * @param request
+ * @param sendResponse
+ */
+function jump(request: any, sendResponse: any) {
+  let shiori = '';
+
+  for (let scrollTopValueByDomain of request.scrollTopValueByDomainList) {
+    if (scrollTopValueByDomain.domain === document.location.href.split('/')[2]) {
+      shiori = scrollTopValueByDomain.scrollTopValue;
+    }
+  }
+
+  // 栞位置までscroll(オートスクロールの場合は4000ms)
+  let bodyHeight: any = $('body').height();
+  if (parseInt(shiori, 10) > bodyHeight) {
+    $('html, body').animate({ scrollTop: shiori }, 4000);
+  } else {
+    $('html, body').animate({ scrollTop: shiori }, 500);
+  }
+
+  // create shiori.
+  let activeShioriElement = $('#shiori-border');
+  if (typeof activeShioriElement !== 'undefined') {
+    activeShioriElement.remove();
+  }
+
+  let shioriElement = $('<img>', {
+    'id': 'shiori-border',
+    'src': chrome.extension.getURL('/images/shiori.png'),
+    'height': '50px'
+  }).appendTo('body');
+
+  $('#shiori-border').css({
+    top: parseInt(shiori),
+    position: 'absolute',
+    opacity: '0.3'
+  });
+
+}
